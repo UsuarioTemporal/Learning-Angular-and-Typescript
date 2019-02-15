8e49369ef854d5fbf7e1c8f5a426f161f71a34e7
UsuarioTemporal <thomtwd@gmail.com>
2019-02-15 17:39:15 -0500

    Alias en los tipos de datos

---
+++ 3-organizacion/src/first/app-1.ts
@@ -0,0 +1,18 @@
+// Union de types
+
+let variable : number | string | boolean = 'asdasd'
+variable = 1
+variable = true
+
+let cursos = ['typescript',true,1,undefined]
+cursos = [1,2]
+cursos=['asdasd','asdasd']
+const funcion = (number_:number=0)=>{
+    return number_
+}
+console.log(funcion());
+console.log(funcion(54));
+let obLiteral = {
+    1: 'typesccript',
+    sd:2
+}
---
+++ 3-organizacion/src/first/app-2.ts
@@ -0,0 +1,4 @@
+// alias en los tipos de datos
+
+let cursoComprado :'Typescript' | 'Angular';
+cursoComprado = 'Angular'
