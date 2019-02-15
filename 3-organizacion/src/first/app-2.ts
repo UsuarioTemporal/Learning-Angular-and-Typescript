3cc14ade29fe040f7ac1e6bff07ae661c8b6cfeb
UsuarioTemporal <thomtwd@gmail.com>
2019-02-15 18:46:12 -0500

    Alias a funciones

--- 3-organizacion/src/first/app-2.ts
+++ 3-organizacion/src/first/app-2.ts
@@ -8,8 +8,15 @@ const comprarCurso = (curso :'Typescript' | 'Angular')=>{
 comprarCurso('Angular')
 console.log(`Curso comprado ${cursoComprado}`);
 
+//alias
 type Curso = 'Typescript' | 'Angular'
+
+
 let cursoComprado_2 : Curso;
 const funcionComprarCurso = (curso:Curso)=>{
+}
+type FuncionCompra = (curso:Curso)=>void;
+
+const comprarCursoDesc: FuncionCompra=(curso) =>{
     
 }
