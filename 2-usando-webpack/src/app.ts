cc823d03b0b5c5e96cf2c74c5f4457fce1eff8c2
UsuarioTemporal <thomtwd@gmail.com>
2019-02-14 22:27:52 -0500

    Modificando el tsconfig e ignorando la carpeta dist

--- 2-usando-webpack/src/app.ts
+++ 2-usando-webpack/src/app.ts
@@ -1,1 +1,18 @@
-console.log('Typescript');
+let message:string = 'EDteam_lat'
+let arrMessages = [
+    {
+        content :'Hello'
+    },
+    {
+        content : 'typescript'
+    }
+]
+
+arrMessages.forEach(element => {
+    console.log(element.content);
+});
+
+console.log(message);
+
+// type:nombre es postfija
+let students:number
