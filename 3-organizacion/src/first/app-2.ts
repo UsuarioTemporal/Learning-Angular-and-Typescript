0662ac290d510e5b5715b0cd09c81303c276a1d2
UsuarioTemporal <thomtwd@gmail.com>
2019-02-15 18:07:02 -0500

    Alias

--- 3-organizacion/src/first/app-2.ts
+++ 3-organizacion/src/first/app-2.ts
@@ -2,3 +2,14 @@
 
 let cursoComprado :'Typescript' | 'Angular';
 cursoComprado = 'Angular'
+const comprarCurso = (curso :'Typescript' | 'Angular')=>{
+    cursoComprado = curso
+}
+comprarCurso('Angular')
+console.log(`Curso comprado ${cursoComprado}`);
+
+type Curso = 'Typescript' | 'Angular'
+let cursoComprado_2 : Curso;
+const funcionComprarCurso = (curso:Curso)=>{
+    
+}
