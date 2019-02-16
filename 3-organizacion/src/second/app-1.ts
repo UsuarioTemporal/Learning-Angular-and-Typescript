197f3f7eae6c1734c00193a252a1ff5aacb615b4
UsuarioTemporal <thomtwd@gmail.com>
2019-02-15 22:52:32 -0500

    tsc | node dist/second/app-1.js

--- 3-organizacion/src/second/app-1.ts
+++ 3-organizacion/src/second/app-1.ts
@@ -1,4 +1,9 @@
-enum Color{
-    VERDE = 'verde',
-    ROJO = 'rojo'
+enum DaysOfTheWeek {
+    SUN,MON,TUE,WED,THU,FRI,SAT
+}
+let day:DaysOfTheWeek
+day = DaysOfTheWeek.FRI
+
+if(day===DaysOfTheWeek.FRI){
+    console.log('si')
 }
