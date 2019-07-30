6d756717a075ada6de93a8b556eecf2f2309d2a3
MaurickThom <thomtwd@gmail.com>
2019-07-29 22:50:02 -0500

    [creacion de un nuevo componente]

--- angular-codigofacilito/anatomia/PruebaAngular8/src/app/title/title.component.ts
+++ angular-codigofacilito/anatomia/PruebaAngular8/src/app/title/title.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core'; // este es el elemento principal para crear compenentes -- este es un decorador
 // este decorador marca una clase como un componente de angular y ademas permite que cofiguremos
 
-@Component({ //configurar los metadatos que describen nuetro compenente
-    select:'title'  // selector para identificar a nuestro componente -- este sera el nombre de la etiqueta que nosotros utilizaremos
+// @Component({ //configurar los metadatos que describen nuetro compenente
+//     select:'mytitle',  // selector para identificar a nuestro componente -- este sera el nombre de la etiqueta que nosotros utilizaremos
+//     template:`
+//         <h1>CF</h1>
+//     `
+
+// })
+@Component({
+    selector:'myTitle',
+    template:`
+        <h1>CF</h1>
+    `
 })
 export class TitleComponent{
 
