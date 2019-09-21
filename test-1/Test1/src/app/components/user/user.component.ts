77e259cb4d3736c2db1856b858ffb3ec82d8d2b6
MaurickThom <thomtwd@gmail.com>
2019-09-20 23:52:50 -0500

    Ng Module

--- test-1/Test1/src/app/components/user/user.component.ts
+++ test-1/Test1/src/app/components/user/user.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
-  private isNotActive = false
-  private isLogged:boolean = false
-  private name:string = 'Thom'
-  private lastName = 'Roman'
-  constructor() { }
-  ngOnInit() {
+  private isNotActive = false;
+  private isLogged: boolean = false;
+  private name: string = "Thom";
+  private lastName = "Roman";
+  // private title:string = ''
+  private items:string[] = ['Toyota','Hyandai','Mercedes']
+  private seleccion:string = this.items[0]
+  private seleccion_2:string = this.items[0]
+  constructor() {}
+  ngOnInit() {}
+  private changeState() {
+    this.isLogged = !this.isLogged;
   }
-  private changeState(){
-     this.isLogged = !this.isLogged
-   }
-
+  log($event){
+    console.log($event)
+  }
 }
