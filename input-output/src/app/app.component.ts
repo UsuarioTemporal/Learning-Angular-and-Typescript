78531044510ca9312a5a5859cc4bfa3034ed1b81
MaurickThom <thomtwd@gmail.com>
2019-09-28 09:41:23 -0500

    update components

--- input-output/src/app/app.component.ts
+++ input-output/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private _http:HttpClient){
+
+  }
+  ngOnInit(): void {
+    this._http.get('https://jsonplaceholder.typicode.com/users').subscribe(reponse=>{
+      this.users = reponse as any
+      console.log(this.users)
+    })
+  }
   title = 'input-output';
+  users = []
+  
 }
---
+++ input-output/src/app/components/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input('data') user:any // si no se coloca ('user') el atributo por defecto será data
+
+}
